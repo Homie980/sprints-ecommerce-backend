ddf3207eaf574b2e5f6f789f75ffa80888f7e65c
homie980 <boodykhaled@gmail.com>
2022-04-12 19:56:11 +0200

    Finished upload routes

--- backend/server.js
+++ backend/server.js
@@ -12,5 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/products", require("./routes/productRoutes"));
+app.use("/api/upload", require("./routes/uploadRoutes"));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
